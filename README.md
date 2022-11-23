68a14c86853dcff861d14b7cfd07b1106c41345b
emkash01 <53668797+emkash01@users.noreply.github.com>
2022-11-23 11:05:04 -0500

    README +appjs

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Lab 3 for Web Application
 
 ## Purpose
 
-The purpose of this repository is to provide a API to allow common database operations CRUD (Create, Read, Update, and Delete recods).
+The purpose of this repository is to provide a API to allow common database operations CRUD (Create, Read, Update, and Delete records). In our case, we made a database system focusing on the types of squirrels found around Brooklyn College. In order to conduct proper research, we decided to add ways to identify and categorize these squirrels. Such forms include an initial ID, which is the order in which we found them, their location, their age, their unique color ID, and whether we caught them eating or not. The color ID is a separate system we made to help categorize the many shades of fur the squirrels could have had, as well as the multiple combinations that can come out of them. If someone is interested in finding out more about a specific squirrel, they can enter the ID number and explore information about it.
 
 ## Tools used
 
@@ -21,3 +21,32 @@ The purpose of this repository is to provide a API to allow common database oper
 - Put (used to update an item) (we'll have information in two places: body [Json] and user [URL])
 
 fixed the error `Error: Cannot find module 'sqlite3'` [link](https://www.sqlitetutorial.net/sqlite-nodejs/connect/)
+
+
+## What each file does
+
+Sqlite Folder - includes installation packages
+Squirrel.db - Contains the database for the squirrels using sqlite
+
+app.js - provides code to help generate the data
+
+index.html - Structures the website with texts and the hyperlinks that can take you to the data
+
+package.json - contains metadata to help install necessary items as well as to run script. Also gives access to npm command
+
+package-lock.json - ensures that the version of your application is working properly and is updates [link](https://www.geeksforgeeks.org/difference-between-package-json-and-package-lock-json-files/#:~:text=package.-,lock.,and%20save%20it%20in%20package.)
+
+
+## instruction to the code (list endpoints and how we got it)
+
+EX: app.get needed to make sure everything in database was printed in table, but if we wanted data of a specific squirrel, the info would be provided of the index of the squirrel submitted.
+
+
+
+
+## Problems/Challenges
+
+We decided to model the endpoints in a way where if we click any of the links, and then decided to open the link via the source code.
+
+Issue: After getting the localhost and an ID one time, if you did it again, it crashed due to a file error. The problem still exists, but to get around thism we decided to open via source code, as it presents the info. To get rid of the crash, we killed the system. 
+
--- app.js
+++ app.js
@@ -3,46 +3,25 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+//app.use(express.json())// makes sure endpoint body reads as json
+
 //Imports SQlite3
 const sqlite3 = require("sqlite3").verbose();
-//Opens/Reads the Database
+//Opens the Database
 var db = new sqlite3.Database('sqlite/squirrel.db');
 
 const squirrelT = 'SELECT * FROM Squirrel WHERE ID=';
 const colorT = 'SELECT * FROM Color WHERE COLORID=';
 
-//endpoint to see information of all squirrels
-app.get('/squirrel', (req, res) => {
-  var results = [];
-  db.serialize(function() {
-    db.each('SELECT * FROM Squirrel', function(err, row) {
-      results.push({id: row.ID, location: row.LOCATION, age: row.AGE, colorid: row.COLORID, eating: row.EATING});
-    }, function() {
-      res.send({'squirrel_info': results});
-    });
-  });
-});
+// db.all(sql, [], (err, rows) => {
+//   if(err) {
+//     throw err;
+//   }
+//   rows.forEach((row) => {
+//     res.send(console.log(row.LOCATION));
+//   });
+// });
 
-//endpoint to see information of color table
-app.get('/color', (req,res) =>{
-  var results = [];
-  db.serialize(function() {
-    db.each('SELECT * FROM Color', function(err, row) {
-      results.push({ColorId: row.COLORID, mainColor: row.PRIMARYCOLOR, secondaryColor: row.SECONDARYCOLOR});
-    }, function() {
-      res.send({'ColorID_info': results});
-    });
-  });
-})
-
-//adds a new squirrel information into the database
-app.post('/squirrel', (req, res) => {
-  var insert = 'INSERT INTO Squirrel (LOCATION, AGE, COLORID, EATING) VALUES (?,?,?,?)';
-  db.run(insert, ['north','old', 4, 'false']);
-  res.send('A new squirrel was added to the list');
-});
-
-//endpoint to retrieve info of one specific squirrel
 app.get('/squirrel/:id', (req, res) => {
   var results = [];
   db.serialize(function() {
@@ -54,7 +33,6 @@ app.get('/squirrel/:id', (req, res) => {
   });
 });
 
-//endpoint to retrieve the squirrel color based on given squirrel colorID
 app.get('/color/:id', (req, res) => {
   var result = [];
   db.serialize(function() {
@@ -66,28 +44,71 @@ app.get('/color/:id', (req, res) => {
   });
 });
 
-//deletes the information of a specific squirrel(WIP)
-app.delete('/squirrel/:id',(req,res) => {
-  var sqrlNum = req.params.id;
-  // var results = [];
-  // db.serialize(function() {
-  //   db.each('DELETE * FROM Squirrel WHERE ID ='+ sqrlNum, function(err, row) {
-  //     results.push({id: row.ID, location: row.LOCATION, age: row.AGE, colorid: row.COLORID, eating: row.EATING});
-  //   }, function() {
-  //     res.send({'Deleted squirrel info of': results});
-  //   });
-  // });
-  db.run('DELETE * FROM Squirrel WHERE ID =',sqrlNum);
-  res.send('squirrel at index '+ sqrlNum +' has been deleted');
+app.get('/squirrel', (req, res) => {
+  var results = [];
+  db.serialize(function() {
+    db.each('SELECT * FROM Squirrel', function(err, row) {
+      results.push({id: row.ID, location: row.LOCATION, age: row.AGE, colorid: row.COLORID, eating: row.EATING});
+    }, function() {
+      res.send({'squirrel_info': results});
+    });
+  });
 });
-//when the code is run if it works this shows in terminal
+
+app.post('/squirrel', (req, res) => {
+  //var newSquirrels = [];
+  //var n1 = [newSquirrels.push(108,'north','old', 4, 'false')];
+  //var n2 = [newSquirrels.push(109,'north','old', 4, 'false')]
+  //newSquirrels.push(114,'north','old', 4, 'false');
+  //newSquirrels.push(115,'north','old', 4, 'false');
+
+  var insert = 'INSERT INTO Squirrel (LOCATION, AGE, COLORID, EATING) VALUES (?,?,?,?)';
+  db.run(insert, ['north','old', 4, 'false']);
+  //db.run(insert, n2);
+  res.send('A new squirrel was added to the list');
+});
+
+//app.get('/',(req,res) => {
+  //res.status(200).send.json(require('squirrel.db'))
+//})
+//making localhost:8080/squirrels endpoint to show data of all squirrels(needs more work)
+// app.get('/squirrel', (req,res) => {
+//   const r_id = req.query.id || '';
+//   const location = req.query.location || '';
+//   const age = req.query.age || '';
+//   const colorID = req.query.colorID || '';
+//   const eating = req.query.eating || '';
+
+//   var results = [];
+//   console.log("Row Id: "+r_id);
+//   //res.send({"results: " results});
+// });
+
+// prints line of a specific row (needs more work)
+// app.post('/squirrel/:id', (req,res) => {
+//   var results = [];
+//   db.serialize(function(req,res){
+//     db.each("SELECT * FROM TABLEONE WHERE rowid ="+req.params.id, function(idk){
+//       results.push({id: HTMLTableRowElement.ID, loc: HTMLTableRowElement.LOCATION, age: HTMLTableRowElement.AGE, colorid: HTMLTableRowElement.COLORID, eating: HTMLTableRowElement.eating});
+//     },function(){
+//       res.send({"results": results});
+//     })
+//   })
+// })
+//app.get("/color:id", function (req, res) {
+  //req. 
+   //res.sendFile(__dirname + "/index.html");no
+//});
+
+//need to add an app.delete endpoint
+
+//need to add an app.put endpoint for updating information
 app.listen(
     PORT, 
     () => console.log(`Example app listening on http://localhost:${PORT}`)
-)
-//default endpoint that sends you to our html file
+);
+
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
-
 
