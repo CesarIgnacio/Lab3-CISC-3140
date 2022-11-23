022c12079b59b654536120102bc9b5a0732c5c7a
ArturRtr <112581047+ArturRtr@users.noreply.github.com>
2022-11-22 22:46:52 -0500

    final draft code

--- app.js
+++ app.js
@@ -3,47 +3,15 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-//app.use(express.json())// makes sure endpoint body reads as json
-
 //Imports SQlite3
 const sqlite3 = require("sqlite3").verbose();
-//Opens the Database
+//Opens/Reads the Database
 var db = new sqlite3.Database('sqlite/squirrel.db');
 
 const squirrelT = 'SELECT * FROM Squirrel WHERE ID=';
 const colorT = 'SELECT * FROM Color WHERE COLORID=';
 
-// db.all(sql, [], (err, rows) => {
-//   if(err) {
-//     throw err;
-//   }
-//   rows.forEach((row) => {
-//     res.send(console.log(row.LOCATION));
-//   });
-// });
-
-app.get('/squirrel/:id', (req, res) => {
-  var results = [];
-  db.serialize(function() {
-    db.each(squirrelT + req.params.id, function(err, row) {
-      results.push({id: row.ID, location: row.LOCATION, age: row.AGE, colorid: row.COLORID, eating: row.EATING});
-    }, function() {
-      res.send({'squirrel_info': results});
-    });
-  });
-});
-
-app.get('/color/:id', (req, res) => {
-  var result = [];
-  db.serialize(function() {
-    db.each(colorT + req.params.id, function(err, row) {
-      result.push({colorid: row.COLORID, primarycolor: row.PRIMARYCOLOR, secondarycolor: row.SECONDARYCOLOR});
-    }, function() {
-      res.send({'color_info': result});
-    });
-  });
-});
-
+//endpoint to see information of all squirrels
 app.get('/squirrel', (req, res) => {
   var results = [];
   db.serialize(function() {
@@ -55,59 +23,69 @@ app.get('/squirrel', (req, res) => {
   });
 });
 
-app.post('/squirrel', (req, res) => {
-  //var newSquirrels = [];
-  //var n1 = [newSquirrels.push(108,'north','old', 4, 'false')];
-  //var n2 = [newSquirrels.push(109,'north','old', 4, 'false')]
-  //newSquirrels.push(114,'north','old', 4, 'false');
-  //newSquirrels.push(115,'north','old', 4, 'false');
+//endpoint to see information of color table
+app.get('/color', (req,res) =>{
+  var results = [];
+  db.serialize(function() {
+    db.each('SELECT * FROM Color', function(err, row) {
+      results.push({ColorId: row.COLORID, mainColor: row.PRIMARYCOLOR, secondaryColor: row.SECONDARYCOLOR});
+    }, function() {
+      res.send({'ColorID_info': results});
+    });
+  });
+})
 
+//adds a new squirrel information into the database
+app.post('/squirrel', (req, res) => {
   var insert = 'INSERT INTO Squirrel (LOCATION, AGE, COLORID, EATING) VALUES (?,?,?,?)';
   db.run(insert, ['north','old', 4, 'false']);
-  //db.run(insert, n2);
   res.send('A new squirrel was added to the list');
 });
 
-//app.get('/',(req,res) => {
-  //res.status(200).send.json(require('squirrel.db'))
-//})
-//making localhost:8080/squirrels endpoint to show data of all squirrels(needs more work)
-// app.get('/squirrel', (req,res) => {
-//   const r_id = req.query.id || '';
-//   const location = req.query.location || '';
-//   const age = req.query.age || '';
-//   const colorID = req.query.colorID || '';
-//   const eating = req.query.eating || '';
+//endpoint to retrieve info of one specific squirrel
+app.get('/squirrel/:id', (req, res) => {
+  var results = [];
+  db.serialize(function() {
+    db.each(squirrelT + req.params.id, function(err, row) {
+      results.push({id: row.ID, location: row.LOCATION, age: row.AGE, colorid: row.COLORID, eating: row.EATING});
+    }, function() {
+      res.send({'squirrel_info': results});
+    });
+  });
+});
 
-//   var results = [];
-//   console.log("Row Id: "+r_id);
-//   //res.send({"results: " results});
-// });
+//endpoint to retrieve the squirrel color based on given squirrel colorID
+app.get('/color/:id', (req, res) => {
+  var result = [];
+  db.serialize(function() {
+    db.each(colorT + req.params.id, function(err, row) {
+      result.push({colorid: row.COLORID, primarycolor: row.PRIMARYCOLOR, secondarycolor: row.SECONDARYCOLOR});
+    }, function() {
+      res.send({'color_info': result});
+    });
+  });
+});
 
-// prints line of a specific row (needs more work)
-// app.post('/squirrel/:id', (req,res) => {
-//   var results = [];
-//   db.serialize(function(req,res){
-//     db.each("SELECT * FROM TABLEONE WHERE rowid ="+req.params.id, function(idk){
-//       results.push({id: HTMLTableRowElement.ID, loc: HTMLTableRowElement.LOCATION, age: HTMLTableRowElement.AGE, colorid: HTMLTableRowElement.COLORID, eating: HTMLTableRowElement.eating});
-//     },function(){
-//       res.send({"results": results});
-//     })
-//   })
-// })
-//app.get("/color:id", function (req, res) {
-  //req. 
-   //res.sendFile(__dirname + "/index.html");no
-//});
-
-//need to add an app.delete endpoint
-
-//need to add an app.put endpoint for updating information
+//deletes the information of a specific squirrel(WIP)
+app.delete('/squirrel/:id',(req,res) => {
+  var sqrlNum = req.params.id;
+  // var results = [];
+  // db.serialize(function() {
+  //   db.each('DELETE * FROM Squirrel WHERE ID ='+ sqrlNum, function(err, row) {
+  //     results.push({id: row.ID, location: row.LOCATION, age: row.AGE, colorid: row.COLORID, eating: row.EATING});
+  //   }, function() {
+  //     res.send({'Deleted squirrel info of': results});
+  //   });
+  // });
+  db.run('DELETE * FROM Squirrel WHERE ID =',sqrlNum);
+  res.send('squirrel at index '+ sqrlNum +' has been deleted');
+});
+//when the code is run if it works this shows in terminal
 app.listen(
     PORT, 
     () => console.log(`Example app listening on http://localhost:${PORT}`)
-);
-
+)
+//default endpoint that sends you to our html file
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
